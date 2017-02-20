6594542db442e87886f1289681bd128e6fdd9474
Khachik Sukiasyan <khachatur125@gmail.com>
2017-02-21 02:01:47 +0400

    Initial commit

---
+++ DownloadAllImages/DownloadAllImages/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DownloadAllImages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 50);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(216, 58);
+            this.textBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter URL";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DownloadAllImages/DownloadAllImages/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DownloadAllImages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  void button1_Click(object sender, EventArgs e)
+        {
+            downloadHtmlToFile(textBox.Text);
+        }
+
+
+
+
+        private void downloadHtmlToFile(string urlString)
+        {
+
+            string desctopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string folderPath = desctopPath + @"\Downloaded images";
+
+            string newFolderPath = folderPath + @"\" + textBox.Text;
+            string newFilePath = newFolderPath + @"\" + textBox.Text + ".html";
+
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+            if (!Directory.Exists(newFolderPath))
+                Directory.CreateDirectory(folderPath);
+
+            FileStream file = new FileStream(newFilePath, FileMode.Create, FileAccess.ReadWrite);
+            StreamWriter writer = new StreamWriter(file);
+            writer.WriteAsync("aaaaaaaaaaaaa");
+            writer.Close();
+        }
+    }
+}
